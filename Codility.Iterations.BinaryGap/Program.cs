0cff68540105a0ef88c75588ed268f2e87afef99
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-03-31 17:36:23 +0200

    Initial

---
+++ Codility.Iterations.BinaryGap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codility.Iterations.BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
