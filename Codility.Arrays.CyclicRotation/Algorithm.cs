6584571952adb0227ce0ee6c9ac513598a76c74c
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-05 13:04:05 +0200

    CyclicRotation

---
+++ Codility.Arrays.CyclicRotation/Algorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Codility.Arrays.CyclicRotation
+{
+    public class Algorithm
+    {
+        public int[] Solution(int[] a, int k)
+        {
+            if (a.Length <= 1 || k == 0 || k % a.Length == 0)
+            {
+                PrintResult(a, k, a);
+                return a;
+            }
+
+            var result = new int[a.Length];
+            Array.Copy(a, result, a.Length);
+
+            for (var i = 0; i < k; i++)
+            {
+                var intermediateResult = new int[result.Length];
+                intermediateResult[0] = result[result.Length - 1];
+
+                for (var j = 0; j < result.Length - 1; j++)
+                {
+                    intermediateResult[j + 1] = result[j];
+                }
+
+                result = intermediateResult;
+            }
+
+            PrintResult(a, k, result);
+            return result;
+        }    
+
+        private void PrintResult(int[] a, int k, int[] result)
+        {
+            Console.WriteLine($"Solution for array {string.Join(',', a)} rotated {k} times is {string.Join(',', result)}");
+        }
+    }
+}
---
+++ Codility.Arrays.CyclicRotation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Codility.Arrays.CyclicRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { 3, 8, 9, 7, 6 }, 3);
+            algorithm.Solution(new int[] { 0, 0, 0 }, 1);
+            algorithm.Solution(new int[] { 1, 2, 3, 4 }, 4);
+            algorithm.Solution(new int[] { 3, 8, 9, 7, 6 }, 3);
+            algorithm.Solution(new int[] { 0, 0, 0 }, 1);
+            algorithm.Solution(new int[] { 1, 2, 3, 4 }, 4);
+        }
+    }
+}
