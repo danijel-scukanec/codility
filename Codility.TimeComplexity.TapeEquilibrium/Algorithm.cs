024fb798e3f8b03e1b4efe597b0013b3a1935e73
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-06 00:29:10 +0200

    TapeEquilibrium

---
+++ Codility.TimeComplexity.TapeEquilibrium/Algorithm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Codility.TimeComplexity.TapeEquilibrium
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {
+            int sumLeft = a[0];
+            int sumRight = 0;
+
+            for (var i = 1; i < a.Length; i++)
+            {
+                sumRight += a[i];
+            }
+
+            int minSum = Math.Abs(sumLeft - sumRight);
+
+            for (var p = 2; p < a.Length; p++)
+            {
+                sumLeft += a[p - 1];
+                sumRight -= a[p - 1];
+                int intermediateSum = Math.Abs(sumLeft - sumRight);
+
+                if (intermediateSum < minSum)
+                {
+                    minSum = intermediateSum;
+                }
+            }
+
+            Console.WriteLine($"Min sum for array {string.Join(',', a)} is {minSum}");
+            return minSum;
+        }
+    }
+}
---
+++ Codility.TimeComplexity.TapeEquilibrium/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Codility.TimeComplexity.TapeEquilibrium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { 4 });
+            algorithm.Solution(new int[] { 4, 2 });
+            algorithm.Solution(new int[] { 3, 1, 2, 4, 3 });
+            algorithm.Solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        }
+    }
+}
