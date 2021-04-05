67ed321de87fd7feba4dea4c1966dd4ac4cd752a
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-05 21:56:06 +0200

    PermMissingElem

---
+++ Codility.TimeComplexity.PermMissingElem/Algorithm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.TimeComplexity.PermMissingElem
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {    
+            var hashtable = new Dictionary<int, bool>();
+
+            for (var i = 0; i < a.Length + 1; i++)
+            {
+                hashtable.Add(i + 1, false);
+            }
+
+            foreach (var element in a)
+            {
+                hashtable[element] = true;
+            }
+
+            var missingElement = hashtable.Single(x => x.Value == false).Key;
+            Console.WriteLine($"Missing element is {missingElement}");
+            
+            return missingElement;
+        }
+    }
+}
---
+++ Codility.TimeComplexity.PermMissingElem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codility.TimeComplexity.PermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] {});
+            algorithm.Solution(new int[] { 2, 3, 1, 5 });
+        }
+    }
+}
