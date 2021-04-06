9e7242ba8022660cf81138fffb86b8dfa009c678
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-06 14:25:57 +0200

    PermCheck

---
+++ Codility.Arrays.PermCheck/Algorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.CountingElements.PermCheck
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {
+            var hashtable = new Dictionary<int, bool>();
+            for(var i = 1; i <= a.Length; i++)
+            {
+                hashtable.Add(i, false);
+            }
+
+            foreach (var element in a)
+            {
+                if (hashtable.ContainsKey(element))
+                {
+                    hashtable[element] = true;
+                }
+            }
+
+            var isPermutation = !hashtable.ContainsValue(false);
+            var result = isPermutation ? 1 : 0;
+
+            Console.WriteLine($"Array {string.Join(',', a)} result of permutation check is {result}");
+            return result;
+        }
+    }
+}
---
+++ Codility.Arrays.PermCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Codility.CountingElements.PermCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { 4, 1, 3, 2 });
+            algorithm.Solution(new int[] { 4, 1, 3 });
+
+        }
+    }
+}
