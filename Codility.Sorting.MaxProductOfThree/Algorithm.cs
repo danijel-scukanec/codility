f8433e101406f0a07f75f43e2ad75aecfa26d9d6
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-06 23:52:33 +0200

    MaxProductOfThree

---
+++ Codility.Sorting.MaxProductOfThree/Algorithm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Codility.Sorting.MaxProductOfThree
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {
+            Array.Sort(a);
+
+            int maxProduct = a[a.Length - 1] * a[a.Length - 2] * a[a.Length - 3];          
+
+            if(a[0] < 0 && a[1] < 0)
+            {
+                var tempResult = a[0] * a[1] * a[a.Length - 1];
+                if(tempResult > maxProduct)
+                {
+                    maxProduct = tempResult;
+                }
+            }                            
+
+            Console.WriteLine($"Max product for array {string.Join(',', a)} is {maxProduct}");
+            return maxProduct;
+        }
+    }
+}
---
+++ Codility.Sorting.MaxProductOfThree/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Codility.Sorting.MaxProductOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { -3, 1, 2, -2, 5, 6 });
+            algorithm.Solution(new int[] { -5, 5, -5, 4 });
+        }
+    }
+}
