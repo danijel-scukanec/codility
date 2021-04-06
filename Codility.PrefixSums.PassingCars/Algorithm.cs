5cd24a593153d32b44aae7f4d709babe75efb2fe
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-06 22:07:52 +0200

    PassingCars

---
+++ Codility.PrefixSums.PassingCars/Algorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.PrefixSums.PassingCars
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {
+            int totalSum = 0;
+            var hashtable = new Dictionary<int, int>();
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 0)
+                {
+                    hashtable[i] = totalSum;
+                }
+                else if (a[i] == 1)
+                {
+                    totalSum += a[i];
+                }
+            }
+
+            int counter = 0;
+            foreach (var keypair in hashtable)
+            {
+                counter += totalSum - keypair.Value;
+
+                if(counter > 1000000000)
+                {
+                    return -1;
+                }
+            }
+
+            Console.WriteLine($"Number of passing cars in array {string.Join(',', a)} is {counter}");
+            return counter;
+        }
+    }
+}
---
+++ Codility.PrefixSums.PassingCars/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Codility.PrefixSums.PassingCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { 0, 1, 0, 1, 1 });
+        }
+    }
+}
