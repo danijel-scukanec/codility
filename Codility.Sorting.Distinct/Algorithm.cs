d7b90dedbab8872c4e8a8f5051f6e13cc716e4b7
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-06 22:51:28 +0200

    Distinct

---
+++ Codility.Sorting.Distinct/Algorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.Sorting.Distinct
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {
+            var hashtable = new Dictionary<int, int>();
+
+            foreach(var element in a)
+            {
+                if (!hashtable.ContainsKey(element))
+                {
+                    hashtable.Add(element, 0);
+                }
+            }
+
+            Console.WriteLine($"Array {string.Join(',', a)} has {hashtable.Count} distinct values");
+            return hashtable.Count;
+        } 
+    }
+}
---
+++ Codility.Sorting.Distinct/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Codility.Sorting.Distinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { 2, 1, 1, 2, 3, 1 });
+        }
+    }
+}
