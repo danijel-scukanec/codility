66ede1ae5c22fa518682d482ad9c7cfdfcf7bb17
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-07 22:58:57 +0200

    Brackets

---
+++ Codility.StackAndQueues.Brackets/Algorithm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.StackAndQueues.Brackets
+{
+    public class Algorithm
+    {
+        public int Solution(string s)
+        {
+            if (s.Length == 0)
+            {
+                Console.WriteLine($"String {s} is properly nested.");
+                return 1;
+            }
+
+            var stack = new Stack<char>();
+            foreach (var currentLetter in s)
+            {
+                if (currentLetter == '(' || currentLetter == '{' || currentLetter == '[')
+                {
+                    stack.Push(currentLetter);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return 0;
+                    }
+
+                    var popedLetter = stack.Pop();
+                    if (popedLetter == '(' && currentLetter != ')' || popedLetter == '{' && currentLetter != '}' || popedLetter == '[' && currentLetter != ']')
+                    {
+                        Console.WriteLine($"String {s} is not properly nested.");
+                        return 0;
+                    }
+                }
+            }
+
+            Console.WriteLine($"String {s} is properly nested.");
+            return stack.Count == 0 ? 1 : 0;
+        }        
+    }
+}
---
+++ Codility.StackAndQueues.Brackets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Codility.StackAndQueues.Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution("{[()()]}");
+            algorithm.Solution("([)()]");
+            algorithm.Solution("{[()()]}{[()()]}");
+        }
+    }
+}
