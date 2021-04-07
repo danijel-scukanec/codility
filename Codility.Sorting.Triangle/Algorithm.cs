f3a3f720f037108b8ad74c1942cf83b6d7d79315
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-07 22:30:11 +0200

    Triangle

---
+++ Codility.Sorting.Triangle/Algorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Codility.Sorting.Triangle
+{
+    public class Algorithm
+    {
+        public int Solution(int[] a)
+        {
+            if (a.Length < 3)
+            {
+                PrintResult(a, 0);
+                return 0;
+            }
+
+            Array.Sort(a);
+            for (var i = 2; i < a.Length; i++)
+            {
+                if ((long)a[i - 2] + a[i - 1] > a[i])
+                {
+                    PrintResult(a, 1);
+                    return 1;
+                }
+            }
+
+            PrintResult(a, 0);
+            return 0;
+        }
+
+        private void PrintResult(int[] a, int result)
+        {
+            Console.WriteLine($"Result of triangle for array {string.Join(',', a)} is {result}");
+        }
+    }
+}
---
+++ Codility.Sorting.Triangle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codility.Sorting.Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+            algorithm.Solution(new int[] { 10, 2, 5, 1, 8, 20 });
+            algorithm.Solution(new int[] { 10, 50, 5, 1 });
+        }
+    }
+}
