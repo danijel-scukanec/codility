085f85398fdc34449b7c04561836aa94831dbdc4
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-10 21:57:38 +0200

    CountFactors

---
+++ Codility.PrimeAndCompositeNumbers.CountFactors/Algorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Codility.PrimeAndCompositeNumbers.CountFactors
+{
+    public class Algorithm
+    {
+        public int SolutionBad(int n)
+        {
+            var counter = 0;
+            for (var i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public int Solution(int n)
+        {
+            var counter = 0;
+            var limit = (int)Math.Sqrt(n);
+
+            for (var i = 1; i <= limit; i++)
+            {
+                if (n % i == 0)
+                {
+                    counter += 2;
+                }
+            }
+
+            if (limit == Math.Sqrt(n))
+            {
+                counter--;
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ Codility.PrimeAndCompositeNumbers.CountFactors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codility.PrimeAndCompositeNumbers.CountFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+
+            var n1 = 24;
+            var result1 = algorithm.Solution(n1);
+            Print(n1, result1);
+        }
+
+        private static void Print(int n, int result)
+        {
+            Console.WriteLine($"Number {n} has {result} factors");
+        }
+    }
+}
