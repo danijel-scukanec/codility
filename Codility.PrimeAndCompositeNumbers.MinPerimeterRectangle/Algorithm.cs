bbb713568c2c0b6ebc5626cf35ba5562914e2dfc
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-10 22:20:46 +0200

    MinPerimeterRectangle

---
+++ Codility.PrimeAndCompositeNumbers.MinPerimeterRectangle/Algorithm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Codility.PrimeAndCompositeNumbers.MinPerimeterRectangle
+{
+    public class Algorithm
+    {
+        public int SolutionGood(int n)
+        {
+            var minPerimeter = int.MaxValue;
+
+            var limit = (int)Math.Sqrt(n);
+            for (var i = 1; i <= limit; i++)
+            {
+                if (n % i == 0)
+                {
+                    var a = i;
+                    var b = n / i;
+                    var perimeter = 2 * (a + b);
+                    minPerimeter = Math.Min(perimeter, minPerimeter);
+                }
+            }
+
+            return minPerimeter;
+        }
+
+        public int SolutionBetter(int n)
+        {
+            var limit = (int)Math.Sqrt(n);
+            for (var i = limit; i > 0; i--)
+            {
+                if (n % i == 0)
+                {
+                    return 2 * (i + n / i);
+                }
+            }
+
+            return int.MaxValue;
+        }
+
+    }
+}
---
+++ Codility.PrimeAndCompositeNumbers.MinPerimeterRectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codility.PrimeAndCompositeNumbers.MinPerimeterRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+
+            var n1 = 30;
+            var result1 = algorithm.SolutionBetter(n1);
+            Print(n1, result1);
+        }
+
+        private static void Print(int n, int result)
+        {
+            Console.WriteLine($"Number {n} has min perimeter {result}");
+        }
+    }
+}
