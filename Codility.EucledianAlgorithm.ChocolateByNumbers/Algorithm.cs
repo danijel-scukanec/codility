98b3226482bdd52e46de3e8b32ad04e379809803
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-11 17:00:20 +0200

    ChocolateByNumbers

---
+++ Codility.EucledianAlgorithm.ChocolateByNumbers/Algorithm.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.EucledianAlgorithm.ChocolateByNumbers
+{
+    public class Algorithm
+    {
+        public int SolutionBad(int n, int m)
+        {
+            var hashtable = new Dictionary<int, bool>();
+
+            for (var i = 0; i < n; i = (i + m) % n)
+            {
+                if (hashtable.ContainsKey(i))
+                {
+                    return hashtable.Count;
+                }
+
+                hashtable.Add(i, true);
+            }
+
+            return 0;
+        }
+
+        public int Solution(int n, int m)
+        {
+            var result = Lcm(n, m) / (long) m;
+            return (int)result;
+        }
+
+        private int Gcd(int a, int b)
+        {
+            if (a % b == 0)
+            {
+                return b;
+            }
+            else
+            {
+                return Gcd(b, a % b);
+            }
+        }
+
+        private long Lcm(int a, int b)
+        {
+            return (long) a * (b / Gcd(a, b));
+        }
+    }
+}
---
+++ Codility.EucledianAlgorithm.ChocolateByNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Codility.EucledianAlgorithm.ChocolateByNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new Algorithm();
+
+            var n1 = 10;
+            var m1 = 4;
+            var result1 = algorithm.Solution(n1, m1);
+            Print(n1, m1, result1);
+
+            var n2 = 10000;
+            var m2 = 999;
+            var result2 = algorithm.Solution(n2, m2);
+            Print(n2, m2, result2);
+
+            var n3 = 1000000000;
+            var m3 = 1000000;
+            var result3 = algorithm.Solution(n3, m3);
+            Print(n3, m3, result3);
+
+            var n4 = 947853;
+            var m4 = 4453;
+            var result4 = algorithm.Solution(n4, m4);
+            Print(n4, m4, result4);
+
+        }
+
+        private static void Print(int n, int m, int result)
+        {
+            Console.WriteLine($"For integers {n} and {m} result is {result}");
+        }
+    }
+}
